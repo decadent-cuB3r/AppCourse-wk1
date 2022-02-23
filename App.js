0b83a258b32188123d24b1aa54a1af1bbc5c034f
阿敗塗一地 <71422011+decadent-cuB3r@users.noreply.github.com>
2022-02-24 02:41:35 +0800

    Personalization

--- App.js
+++ App.js
@@ -1,20 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Image, StatusBar, SafeAreaView } from "react-native";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{flex: 1}}>
+      <StatusBar />
+      <View style={styles.headerStyle}>
+        <Text style={styles.textStyle}>三葉小六與我</Text>
+      </View>
+      <View style={styles.cardContainerStyle}>
+        <View style={styles.thumbnailContainerStyle}>
+          <Image
+            style={styles.thumbnailStyle}
+            source={require('./assets/Me.jpg')}
+          />
+          <View style={styles.headerContentStyle}>
+            <Text>110819022</Text>
+            <Text>Ezra</Text>
+          </View>
+        </View>
+        <View>
+          <Image
+            style={styles.imageStyle}
+            source={require("./assets/Me.jpg")}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  headerStyle: {
+    backgroundColor: "#F8F8F8",
+    justifyContent: "center",
+    alignItems: "center",
+    height: 60,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    // Android Only
+    elevation: 2,
+  },
+  textStyle: {
+    fontSize: 20,
+  },
+  thumbnailContainerStyle: {
+    flexDirection: "row",
+    justifyContent: "flex-start",
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50,
+  },
+  headerContentStyle: {
+    flexDirection: "column",
+    justifyContent: "space-around",
+    paddingLeft: 10,
+  },
+  cardContainerStyle: {
+    borderWidth: 1,
+    borderRadius: 2,
+    borderColor: "#ddd",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+    elevation: 1,
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 10,
+  },
+  imageStyle: {
+    height: 300,
+    width: null,
   },
 });
+
+export default App;
